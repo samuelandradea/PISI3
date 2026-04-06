b662ec1c437c6f24611b31529d8903f4f8090367
Gabryel Gomes Silva Souza <gabryelgomes3030@gmail.com>
2026-04-06 16:17:25 -0300

    Adiciona busca inteligente por prefixo

--- repositories/search_repository.py
+++ repositories/search_repository.py
@@ -10,26 +10,40 @@ def search_in_firebase(termo: str):
     if not termo:
         return resultados
 
-    # 1. BUSCANDO USUÁRIOS (Coleção: users | Campo: name)
+    
+    # 1. Arruma o texto colocando a primeira letra de cada palavra maiúscula
+    termo_formatado = termo.title()
+    
+    # 2. Cria o limite final da busca com o caractere mágico do Firebase
+    limite = termo_formatado + '\uf8ff'
+
+    # ---------------------------------------------------------
+    # 1. BUSCANDO USUÁRIOS
+    # Procura nomes que comecem com as letras digitadas
+    # ---------------------------------------------------------
     users_ref = db.collection('users')
-    query_users = users_ref.where('name', '==', termo).stream()
+    query_users = users_ref.where('name', '>=', termo_formatado).where('name', '<=', limite).stream()
     
     for doc in query_users:
         user_data = doc.to_dict()
         user_data['id'] = doc.id 
         resultados["usuarios"].append(user_data)
 
-    # 2. BUSCANDO LIVROS (Coleção: books | Campo: title)
+    # ---------------------------------------------------------
+    # 2. BUSCANDO LIVROS 
+    # ---------------------------------------------------------
     books_ref = db.collection('books')
-    query_books = books_ref.where('title', '==', termo).stream()
+    query_books = books_ref.where('title', '>=', termo_formatado).where('title', '<=', limite).stream()
     
     for doc in query_books:
         book_data = doc.to_dict()
         book_data['id'] = doc.id
         resultados["livros"].append(book_data)
 
-    # 3. BUSCANDO AUTORES (Coleção: books | Campo: authors)
-    query_authors = books_ref.where('authors', '==', termo).stream()
+    # ---------------------------------------------------------
+    # 3. BUSCANDO AUTORES
+    # ---------------------------------------------------------
+    query_authors = books_ref.where('authors', '>=', termo_formatado).where('authors', '<=', limite).stream()
     
     for doc in query_authors:
         author_data = doc.to_dict()
