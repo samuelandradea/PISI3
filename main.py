de1f8401eb2158165efd56c97dff21547c46764f
Matheus Cintra <matheushcintra2007@gmail.com>
2026-05-01 22:06:55 -0300

    Correção da main

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.include_router(search_router)
 app.include_router(review_router)
 app.include_router(book_routes)
 app.include_router(recovery_router)
-app.include_router(book_routes.router)
+app.include_router(book_routes)
 app.include_router(list_router)
 @app.get("/")
 def root():
